15185b83ba8d76c296cabb9de24ecbdbaa697065
Leanne <leannemroz@gmail.com>
2022-01-16 01:41:58 -0600

    create file

---
+++ nuke-connected.js
@@ -0,0 +1,10 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+	let connected = ns.scan(ns.getHostname());
+	while (connected != 0) {
+		let val = connected.pop();
+		if (ns.getServerNumPortsRequired(val) == 0) {
+			await ns.nuke(val);
+		}
+	}
+}
