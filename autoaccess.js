c5ff69172a81cdde10df3e4fa2ae69ec243af5f7
Leanne <leannemroz@gmail.com>
2022-01-16 00:52:05 -0600

    create autoaccess.js w/ outline of functionality

---
+++ autoaccess.js
@@ -0,0 +1,11 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+    /** How to gain entry: 
+     * 1. run scan-analyze
+     * 1a. get # of all hosts available, set that as the loop #
+     * 2. connect [hostname]
+     * 3. if no ports needed to NUKE, then run NUKE.exe
+     * 4. if [x] port needed to NUKE, run BruteSSH.exe, FTPCrack.exe, etc.
+     * 5. then, hack as needed
+     */
+}
